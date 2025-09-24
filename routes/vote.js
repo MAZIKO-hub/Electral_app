63cdfef109802a05e1ba75a975d995d2bf902727
MAZIKO <tilingamawamaziko@gmail.com>
2025-09-25 00:04:00 +0200

    Added register js and sql to db added 404 view modified server to do post

--- routes/vote.js
+++ routes/vote.js
@@ -1,30 +1,19 @@
 const express = require('express');
-module.exports =(dbs) =>{
+module.exports = (dbs) => {
     const router = express.Router();
-    router.get('/',(req,res)=>{
-        res.render("vote");
-    });
-    router.get('/vote',(req,res)=>{
+
+    router.get('/', (req, res) => {
         res.render("vote");
     });
 
-    router.get('/register',(req,res)=>{
+    router.get('/vote', (req, res) => {
+        res.render("vote");
+    });
+
+    router.get('/register', (req, res) => {
         res.render("register");
     });
-    // router.post('/add',(req,res)=>{
-    //     const {name, party} = req.body;
-    //     if(!name || !party){
-    //         return res.status(400).send('Name and Party are required');
-    //     }
 
-    //     const sql = 'INSERT INTO candidates (name, party) VALUES (?, ?)';
-    //     dbs.query(sql, [name, party], (err, result) => {
-    //         if (err) {
-    //             return res.status(500).send('Database error');
-    //         }
-    //         res.redirect('/candidates');
-    //     });
-    // });
 
     router.get('/cast_vote',(req,res)=>{
         res.render("cast_vote");
--- server.js
+++ server.js
@@ -14,32 +14,69 @@ const dbs = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'electral_app_dbs'    
-})
+    database: 'electral_app_dbs'
+});
 
+const vote_routes = require('./routes/home');
+app.use('/', vote_routes);
 
-dbs.connect(error=>{
-    if(error){
+const candidate_routes = require('./routes/candidates')(dbs);
+app.use('/candidates', candidate_routes);
+
+const vote = require('./routes/vote')(dbs);
+app.use('/vote', vote);
+
+app.post("/voter/register", (req, res) => {
+    const { nationalID, firstName, surname, gender, age, status, pollingStationID } = req.body;
+
+    const insertPerson = `
+        INSERT INTO Person (NationalID, FirstName, Surname, Gender, Age)
+        VALUES (?, ?, ?, ?, ?)
+        ON DUPLICATE KEY UPDATE 
+            FirstName = VALUES(FirstName),
+            Surname = VALUES(Surname),
+            Gender = VALUES(Gender),
+            Age = VALUES(Age)
+    `;
+
+    dbs.query(insertPerson, [nationalID, firstName, surname, gender, age], (err) => {
+        if (err) {
+            console.error("Error inserting into Person:", err);
+            return res.status(500).send("Error saving person.");
+        }
+
+        const insertVoter = `
+            INSERT INTO Voter (Status, NationalID, PollingStationID)
+            VALUES (?, ?, ?)
+        `;
+
+        dbs.query(insertVoter, [status, nationalID, pollingStationID], (err) => {
+            if (err) {
+                console.error("Error inserting into Voter:", err);
+                return res.status(500).send("Error saving voter.");
+            }
+
+            console.log("Voter registered:", nationalID);
+            res.redirect("/vote");
+        });
+    });
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.use((req, res) => {
+    res.status(404).render('404', { title: 'Page Not Found' });
+});
+
+dbs.connect(error => {
+    if (error) {
         console.error('database could not connect: ' + error.stack);
         process.exit(1);
     }
     console.log(`database connected ${dbs.threadId}`);
-
-    const vote_routes = require('./routes/home');
-    app.use('/', vote_routes);
-    const candidate_routes = require('./routes/candidates')(dbs);
-    app.use('/candidates', candidate_routes);
-    const vote = require('./routes/vote')(dbs);
-    app.use('/vote', vote);  
-
-    app.use((err, req, res, next) => {
-        console.error(err.stack);
-        res.status(500).send('Something broke!');
-    });
-
-    app.use((req, res) => {
-        res.status(404).render('404', { title: 'Page Not Found' }); 
-    });
 
     app.listen(PORT, () => {
         console.log(`Server running at http://localhost:${PORT}`);
